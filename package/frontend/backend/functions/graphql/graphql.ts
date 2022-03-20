dfcbe9a24f22488b953146da9a73228c416f8917
alpha2phi <alpha2phi@gmail.com>
2022-03-20 19:44:02 +0800

    Update graphql

--- package/frontend/backend/functions/graphql/graphql.ts
+++ package/frontend/backend/functions/graphql/graphql.ts
@@ -4,25 +4,58 @@ import {
   Config,
 } from "apollo-server-core";
 import { ApolloServer, gql } from "apollo-server-lambda";
+import dynamoDb from "../../core/dynamodb";
+import context from "../../core/context";
+import { GetItemInput, ScanInput } from "aws-sdk/clients/dynamodb";
 
 const IS_LOCAL = !!process.env.IS_LOCAL;
 
+const STOCKS_TABLE = context.getResource(process.env.STOCKS_TABLE);
+
 const typeDefs = gql`
+  type Stock {
+    country: String!
+    symbol: String!
+    currency: String
+    full_name: String
+    isin: String
+    name: String
+  }
+
   type Query {
     hello: String
-    stocks: String
+    stocks(country: String, symbol: String): [Stock]
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Hello, it is working!",
-    stocks: async (parent, args, context, info) => {
-      console.dir(parent);
-      console.dir(args);
-      console.dir(context);
-      console.dir(info);
-      return "hello stocks";
+
+    stocks: async (parent, args, ctx, info) => {
+      if (args.country && args.symbol) {
+        const params: GetItemInput = {
+          TableName: STOCKS_TABLE,
+          Key: {
+            country: args.country.toLowerCase(),
+            symbol: args.symbol.toUpperCase(),
+          },
+        };
+        const result = await dynamoDb.get(params);
+        if (result.Item) {
+          return Array.of(result.Item);
+        } else {
+          return [];
+        }
+      } else {
+        const params: ScanInput = {
+          TableName: STOCKS_TABLE,
+          Select: "ALL_ATTRIBUTES",
+          Limit: 10,
+        };
+        const result = await dynamoDb.scan(params);
+        return result.Items;
+      }
     },
   },
 };
