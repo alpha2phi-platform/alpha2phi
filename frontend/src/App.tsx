bc59528718bfcff0c87bd5e82b9f7f7a1865604a
alpha2phi <alpha2phi@gmail.com>
2022-04-10 23:12:48 +0800

    Refactor frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,47 @@
 import React from "react";
-import Slider from "@mui/material/Slider";
+import { useStockQuery, useStocksQuery } from "./data/urql";
+import "./App.css";
+
+const ListStocks = () => {
+  const [{ data, fetching, error }] = useStocksQuery();
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <div>
+      {data?.listStocks?.map((stock) => (
+        <div key={stock?.symbol}>
+          {stock?.symbol} - {stock?.name}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const GetStock = ({ country, symbol }) => {
+  const [{ data, fetching, error }] = useStockQuery({
+    variables: {
+      country: country,
+      symbol: symbol,
+    },
+  });
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <div>
+      {data?.getStockBySymbol?.symbol} - {data?.getStockBySymbol?.name}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <Slider defaultValue={30} />
-      <Slider
-        defaultValue={30}
-        className="text-teal-600"
-        componentsProps={{ thumb: { className: "rounded-sm" } }}
-      />
+    <div className="App">
+      {/* <GetStock country="united states" symbol="aapl" /> */}
+      <ListStocks />
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,28 +4,28 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import "./index.css";
 import App from "./App";
-// import { createClient, Provider, defaultExchanges } from "urql";
+import { createClient, Provider, defaultExchanges } from "urql";
 
-// const client = createClient({
-//   url: import.meta.env.VITE_GRAPHQL_URL,
-//   exchanges: defaultExchanges,
-//   fetchOptions: () => {
-//     return {
-//       headers: {
-//         "x-api-key": import.meta.env.VITE_GRAPHQL_API_KEY,
-//       },
-//     };
-//   },
-// });
+const client = createClient({
+  url: import.meta.env.VITE_GRAPHQL_URL,
+  exchanges: defaultExchanges,
+  fetchOptions: () => {
+    return {
+      headers: {
+        "x-api-key": import.meta.env.VITE_GRAPHQL_API_KEY,
+      },
+    };
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Provider value={client}> */}
-    <StyledEngineProvider injectFirst>
-      <CssBaseline />
-      <App />
-    </StyledEngineProvider>
-    {/* </Provider> */}
+    <Provider value={client}>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <App />
+      </StyledEngineProvider>
+    </Provider>
   </React.StrictMode>
 );
