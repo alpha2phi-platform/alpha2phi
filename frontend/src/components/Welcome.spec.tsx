9912c67876cba4ec3b812a86d9b4e20874cbd1e0
alpha2phi <alpha2phi@gmail.com>
2022-04-10 23:40:36 +0800

    Refactor frontend

--- frontend/src/components/Welcome.spec.tsx
+++ frontend/src/components/Welcome.spec.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { mount } from "@cypress/react";
 import Welcome from "./Welcome";
 
-it("render stocks", () => {
+it("render welcome", () => {
   mount(<Welcome />);
-  cy.get("h1").should("have.text", "Hello");
+  cy.get("h1").contains("Hello");
 });
--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -3,5 +3,5 @@ import React from "react";
 
 export default function Welcome(props) {
   // Display the name passed as props
-  return <h1>Hello, {props.name}</h1>;
+  return <h1>Hello {props.name}</h1>;
 }
